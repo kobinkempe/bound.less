9e12d2b7cdd1cf8a3a46ced7b0ad81f6479b2984
Kobin Kempe <kobinkempe@gmail.com>
2021-02-14 02:22:12 -0600

    Comments to redux

--- boundless/src/Redux/store.js
+++ boundless/src/Redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import reducerOne from './reducerSliceOne';
 
+//Here is where we store the state information. This combines all the reducers we may want to use - KK
 export default configureStore({
     reducer: {
         counter: reducerOne
