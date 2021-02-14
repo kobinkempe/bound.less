cf91b00bcc714d874785e085710a48e05e24f34f
nolanraghu <nolan.raghu@vanderbilt.edu>
2021-02-13 22:21:11 -0600

    tried to implement some stuff...again

--- boundless/src/App.js
+++ boundless/src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import heyworld from "./oleTom.js";
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <heyworld/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
