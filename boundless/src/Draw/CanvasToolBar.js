d3d0c6e55915445b9b011e3a5c4ef4a2f049e582
Kobin Kempe <kobinkempe@gmail.com>
2021-03-24 13:44:10 -0500

    separated canvasToolbar

--- boundless/src/Draw/CanvasToolBar.js
+++ boundless/src/Draw/CanvasToolBar.js
@@ -1,17 +1,189 @@
-import {useHistory, useParams, Link} from "react-router-dom";
-import TwoCanvas from "../Draw/TwoCanvas";
+import {ClickAwayListener, Fab} from "@material-ui/core";
+import {
+    AllOut,
+    BorderColorRounded,
+    Close,
+    DeleteForever,
+    FormatShapes,
+    Height,
+    Palette,
+    Undo,
+    Work
+} from "@material-ui/icons";
+import LogoSmallIcon from "../Images/toolbarIcons/logoSmall";
 import {HexColorPicker} from "react-colorful";
-import {useDispatch, useSelector} from "react-redux";
-import {changeColorPen, selectRGB} from "../Redux/rSlicePenOptions";
-import {Box, Button, ButtonGroup, ClickAwayListener, Portal} from "@material-ui/core";
 import styles from "../Stylesheets/CanvasPage.css";
-import "../Stylesheets/CanvasToolBar.css";
-import {logIn, logOut, selectLoggedIn} from "../Redux/loginState";
+import WidthSlider from "./Toolbar/WidthSlider";
 import {useState} from "react";
+import {useHistory} from "react-router-dom";
 
 
-export const CanvasToolBar = () => {
+export default function CanvasToolbar({setToolSelected,
+                                      selectColor,
+                                      setSelectColor,
+                                      setWipe,
+                                      lineWidth,
+                                      setLineWidth,
+                                      setUndoState}){
+    //Toolbar States
+    const [toolDisplay, setToolDisplay] = useState('closed');
+
+    //Palette
+    const [openPalette, setOpenPalette] = useState(false);
+
+    //Height parameter
+    const [openLineWidth, setOpenLineWidth] = useState(false);
+
+    const history = useHistory();
+
+    //Realtime Database stupid thing
+    // const [i, update] = useState(0);
+
+
+    let cleanup = () => {
+        setWipe(false);
+
+    }
+
+    let logoIcon =
+        <Fab className={'tool'}
+             onClick={()=>{
+                 history.push('/');
+             }}>
+            <LogoSmallIcon/>
+        </Fab>;
+
+    let openToolBar = [
+        //Undo
+        <Fab className={'tool'}
+             onClick={()=>{
+                 setUndoState(true)
+             }}>
+            <Undo/>
+        </Fab>,
+
+
+
+        //Pen
+        <Fab className={'tool'}
+             onClick={()=>{
+                 cleanup();
+                 setToolSelected('pen')
+             }}>
+            <BorderColorRounded/>
+        </Fab>,
+        //Palette
+        <Fab className={'tool'}
+             onClick={()=>{
+                 if(openPalette){
+                     setOpenPalette(false);
+                 } else {
+                     setOpenPalette(true);
+                     //setToolDisplay('palette');
+                 }
+             }}>
+            <Palette/>
+        </Fab>,
+
+        //TextBox
+        <Fab className={'tool'}
+             onClick={()=>{
+                 cleanup();
+                 setToolSelected('rectangle')
+             }}>
+            <FormatShapes/>
+        </Fab>,
+
+        //Weird Circle/Square thing
+        <Fab className={'tool'}
+             onClick={()=>{
+                 cleanup();
+                 setToolSelected('circle')
+             }}>
+            <AllOut/>
+        </Fab>,
+
+
+        //LineWidth
+        <Fab className={'tool'}
+             onClick={() => {
+                 cleanup();
+                 if(openLineWidth) {
+                     setOpenLineWidth(false);
+                 }else {
+                     setOpenLineWidth(true);
+                 }
+             }
+             }>
+            <Height/>
+        </Fab>,
+
+        //Wipe Canvas Button
+        <Fab className={'tool'}
+             onClick={()=>{
+                 setOpenLineWidth(false);
+                 // addCanvas(i);
+                 // addCanvas(i + 'private', "John Doe", false);
+                 // setTimeout(()=>{
+                 //     canAccessCanvas(i + 'private', (ret)=>{console.log(ret)});
+                 //     canAccessCanvas(i, ret => console.log(ret), 'Fred');
+                 // }, 500);
+                 // setTimeout(()=>{
+                 //     makeCanvasPrivate(i, "John Doe", ()=>console.log("pass1"), ()=>console.log("fail1"))
+                 // }, 5000);
+                 // setTimeout(()=>{
+                 //     removeCanvas(i, "John Doe", ()=>{console.log("pass2")}, ()=>{console.log("fail2")})
+                 // }, 10000);
+                 // update(i + 1);
+                 setWipe(true);
+             }}>
+            <DeleteForever/>
+        </Fab>,
+
+        //X
+        <Fab className={'tool'} size={'small'}
+             onClick={()=>{
+                 cleanup();
+                 setToolDisplay('closed')}}>
+            <Close/>
+        </Fab>
+    ];
+
+    //The different popups being used in the toolbar
+    let colorPicker =
+        <ClickAwayListener onClickAway={()=>{setOpenPalette(false)}}>
+            <div className='colorPickerWrapperC'>
+                <HexColorPicker className={styles.small}
+                                color={selectColor}
+                                onChange={setSelectColor}
+                />
+            </div>
+        </ClickAwayListener>
+
+    let heightSlider =
+        <div className={'hSlider'}>
+            <WidthSlider onClick={setLineWidth} lineWidth={lineWidth}/>
+        </div>
+
+
+    switch (toolDisplay) {
+        case 'closed':
+            return (
+                [
+                    logoIcon,
+                    <Fab className={'tool'} onClick={()=>{setToolDisplay('open')}}>
+                        <Work/>
+                    </Fab>
+                ]
+            )
+        case 'open':
+            return [
+                logoIcon,
+                openToolBar,
+                (openPalette? colorPicker: null),
+                (openLineWidth? heightSlider:null)
+            ];
+        default:
+            return;
+    }
 }
-
-export default CanvasToolBar;
-
--- boundless/src/Pages/CanvasPage.js
+++ boundless/src/Pages/CanvasPage.js
@@ -25,47 +25,30 @@ import LogoSmallIcon from "../Images/toolbarIcons/logoSmall";
 import {addCanvas, canAccessCanvas, makeCanvasPrivate, removeCanvas} from "../Firebase";
 
 import WidthSlider from "../Draw/Toolbar/WidthSlider";
+import CanvasToolbar from "../Draw/CanvasToolBar";
 
+// function getRandomColor() {
+//     return 'rgb('
+//         + Math.floor(Math.random() * 255) + ','
+//         + Math.floor(Math.random() * 255) + ','
+//         + Math.floor(Math.random() * 255) + ')';
+// }
 
-
-
-
-
-function getRandomColor() {
-    return 'rgb('
-        + Math.floor(Math.random() * 255) + ','
-        + Math.floor(Math.random() * 255) + ','
-        + Math.floor(Math.random() * 255) + ')';
-}
 export const CanvasPage = () => {
     let {canvasID} = useParams();
     const history = useHistory();
     const dispatch = useDispatch();
 
-
     //Login States
     const loggedIn = useSelector(selectLoggedIn);
     const [loggingIn, setLoggingIn] = useState(false);
 
-    //Toolbar States
-    const [toolDisplay, setToolDisplay] = useState('closed');
+    //Tool States
     const [toolSelected, setToolSelected] = useState('pen');
-
-    //Palette
-    const [openPalette, setOpenPalette] = useState(false);
     const [selectColor, setSelectColor] = useState('rgb(0,0,0)');
-    //const selectColor = useSelector(selectRGB);
-
-    //Clear Screen
     const [wipe, setWipe] = useState(false);
-    // const [i, update] = useState(0);
-
-    //undo
-    const [undoState, setUndoState] = useState(false);
-
-    //Height parameter
     const [lineWidth, setLineWidth] = useState(2);
-    const [openLineWidth, setOpenLineWidth] = useState(false);
+    const [undoState, setUndoState] = useState(false);
 
     let onPressButton;
     if(loggedIn){
@@ -76,16 +59,6 @@ export const CanvasPage = () => {
         onPressButton = ()=>{
             setLoggingIn(true);
         }
-    }
-
-
-
-    /** I have a feeling that a lot of extra setting are going to be needed to be cleared between tools, so I've
-     * started a "cleanup" function that's called whenever tools are switched**/
-
-    let cleanup = () => {
-        setWipe(false);
-
     }
 
     let loginBox = () => {
@@ -104,152 +77,6 @@ export const CanvasPage = () => {
                 </div>
             </div>
         )
-    }
-
-    let logoIcon =
-        <Fab className={'tool'}
-             onClick={()=>{
-                 history.push('/');
-             }}>
-            <LogoSmallIcon/>
-        </Fab>;
-
-    let openToolBar = [
-        //Undo
-        <Fab className={'tool'}
-             onClick={()=>{
-                setUndoState(true)
-             }}>
-            <Undo/>
-        </Fab>,
-
-
-
-        //Pen
-        <Fab className={'tool'}
-             onClick={()=>{
-                 cleanup();
-                 setToolSelected('pen')
-             }}>
-            <BorderColorRounded/>
-        </Fab>,
-        //Palette
-        <Fab className={'tool'}
-             onClick={()=>{
-                 if(openPalette){
-                     setOpenPalette(false);
-                 } else {
-                     setOpenPalette(true);
-                     //setToolDisplay('palette');
-                 }
-             }}>
-            <Palette/>
-        </Fab>,
-
-        //TextBox
-        <Fab className={'tool'}
-             onClick={()=>{
-                 cleanup();
-                 setToolSelected('rectangle')
-             }}>
-            <FormatShapes/>
-        </Fab>,
-
-        //Weird Circle/Square thing
-        <Fab className={'tool'}
-             onClick={()=>{
-                 cleanup();
-                 setToolSelected('circle')
-             }}>
-            <AllOut/>
-        </Fab>,
-
-
-        //LineWidth
-        <Fab className={'tool'}
-             onClick={() => {
-                 cleanup();
-                 if(openLineWidth) {
-                     setOpenLineWidth(false);
-                 }else {
-                    setOpenLineWidth(true);
-                    }
-                 }
-             }>
-            <Height/>
-        </Fab>,
-
-        //Wipe Canvas Button
-        <Fab className={'tool'}
-             onClick={()=>{
-                 setOpenLineWidth(false);
-                 // addCanvas(i);
-                 // addCanvas(i + 'private', "John Doe", false);
-                 // setTimeout(()=>{
-                 //     canAccessCanvas(i + 'private', (ret)=>{console.log(ret)});
-                 //     canAccessCanvas(i, ret => console.log(ret), 'Fred');
-                 // }, 500);
-                 // setTimeout(()=>{
-                 //     makeCanvasPrivate(i, "John Doe", ()=>console.log("pass1"), ()=>console.log("fail1"))
-                 // }, 5000);
-                 // setTimeout(()=>{
-                 //     removeCanvas(i, "John Doe", ()=>{console.log("pass2")}, ()=>{console.log("fail2")})
-                 // }, 10000);
-                 // update(i + 1);
-                 setWipe(true);
-             }}>
-            <DeleteForever/>
-        </Fab>,
-
-        //X
-        <Fab className={'tool'} size={'small'}
-             onClick={()=>{
-                 cleanup();
-                 setToolDisplay('closed')}}>
-            <Close/>
-        </Fab>
-    ];
-
-    //The different popups being used in the toolbar
-    let colorPicker =
-        <ClickAwayListener onClickAway={()=>{setOpenPalette(false)}}>
-            <div className='colorPickerWrapperC'>
-                <HexColorPicker className={styles.small}
-                                color={selectColor}
-                                onChange={setSelectColor}
-                />
-            </div>
-        </ClickAwayListener>
-
-    let heightSlider =
-        <div className={'hSlider'}>
-            <WidthSlider onClick={setLineWidth} lineWidth={lineWidth}/>
-        </div>
-
-
-
-
-    let toolBar = () => {
-        switch (toolDisplay) {
-            case 'closed':
-                return (
-                    [
-                        logoIcon,
-                        <Fab className={'tool'} onClick={()=>{setToolDisplay('open')}}>
-                            <Work/>
-                        </Fab>
-                    ]
-                )
-            case 'open':
-                return [
-                    logoIcon,
-                    openToolBar,
-                    (openPalette? colorPicker: null),
-                    (openLineWidth? heightSlider:null)
-                ];
-            default:
-                return;
-        }
     }
 
     let loginButton = () => {
@@ -283,7 +110,13 @@ export const CanvasPage = () => {
     return (
         <div>
             <div color={"primary"} className={'toolbar'} >
-                {toolBar()}
+                <CanvasToolbar selectColor={selectColor}
+                               setLineWidth={setLineWidth}
+                               lineWidth={lineWidth}
+                               setToolSelected={setToolSelected}
+                               setSelectColor={setSelectColor}
+                               setUndoState={setUndoState}
+                               setWipe={setWipe}/>
             </div>
             <TwoCanvas
                 toolInUse={toolSelected}
