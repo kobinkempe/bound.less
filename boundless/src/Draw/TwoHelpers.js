16e014f3744d0e203d6fd86c573609dc3dca1790
nolanraghu <nolan.raghu@vanderbilt.edu>
2021-03-29 00:33:29 -0500

    Changed Undo Limit to 30
    The undo limit of 10 was surprisingly noticeable when playing around with it.

--- boundless/src/Draw/TwoHelpers.js
+++ boundless/src/Draw/TwoHelpers.js
@@ -18,7 +18,7 @@ import {selectLoggedIn} from "../Redux/loginState";
 
 
 
-const UNDO_LIMIT = 10;
+const UNDO_LIMIT = 30;
 
 export const useNumUndos = (initialAmount) => {
     const [num, setNum] = useState(initialAmount);
