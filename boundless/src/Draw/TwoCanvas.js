374ff9958c8787c87766f9e0914846102e8c0552
nolanraghu <nolan.raghu@vanderbilt.edu>
2021-03-29 00:46:45 -0500

    Update TwoCanvas.js

--- boundless/src/Draw/TwoCanvas.js
+++ boundless/src/Draw/TwoCanvas.js
@@ -185,6 +185,9 @@ const TwoCanvas = ({toolInUse,
         setMouse(undefined);
         setTouchInUse(false);
         pushUndoQueue(PGroup);
+        if(PGroup > 1){
+            console.log("Path pushed to undo queue of path size: "+PGroup);
+        }
         //dispatch(loadUndo( PGroup));
         setPGroup(0);
 
