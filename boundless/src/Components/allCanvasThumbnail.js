cc1fa155338f4ae5ac992823fce72bfad0aefd26
anujamehta <44733188+anujamehta@users.noreply.github.com>
2021-04-24 16:46:33 -0500

    Doesn't work 2.0 but it's with hooks

--- boundless/src/Components/allCanvasThumbnail.js
+++ boundless/src/Components/allCanvasThumbnail.js
@@ -3,57 +3,64 @@ import CanvasThumbnail from "./canvasThumbnail";
 import {selectLoggedIn, selectUsername} from "../Redux/loginState";
 import firebase from 'firebase'
 import {useSelector} from "react-redux";
+import React, {useState, useEffect} from 'react';
 
 export default function AllCanvasThumbnail() {
 
-    let canvases = [CanvasThumbnail({href: '#/canvas/new', text: 'New Canvas', newCanvas: true})]
-
+    let canvases = [CanvasThumbnail({href: '#/canvas/new', text: 'New Canvas', newCanvas: true})];
     const username = useSelector(selectUsername);
-    let count = 1;
-    let canvasList;
 
-    let databaseRef = firebase.database().ref();
-
-    const loadfromDB = async () => {
-
+    const loadFromDB = async (databaseRef) => {
+        let count = 1;
+        console.log("Loading from DB");
         databaseRef.child("users").child(username).once('value', (snapshot) => {
             snapshot.forEach((childSnapshot) => {
                 let childKey = childSnapshot.key;
                 canvases.push(CanvasThumbnail({
                     href: '#/canvas/' + childKey,
-                    text: 'Canvas' + count,
+                    text: 'Canvas ' + count,
                     newCanvas: false
                 }))
                 ++count;
             });
-        }).then(() => {
-            console.log("Finished");
+        }).then(()=> {
+            console.log("Finished loading");
         }).catch((error) => {
             console.error(error);
         });
     }
 
-    const loadList = async (canvases) => {
-        await loadfromDB().then(() => {
-            console.log(canvases.length);
-            return canvases.map((canvas, index) => {
-                return <div key={index}>{canvas}</div>
-            });}
-        )
-    }
+    const [canvasList, setCanvasList] = useState([]);
 
-    canvasList = loadList(canvases);
+    useEffect(() => {
+        let databaseRef = firebase.database().ref();
+        loadList(databaseRef);
+    }, []);
+
+    const loadList = async (databaseRef) => {
+        loadFromDB(databaseRef).then(() => {
+            console.log("Load List: " + canvases.length);
+            setCanvasList(canvases.map((canvas, index) => {
+                return (<div key={index}>{canvas}</div>)
+            }));
+            console.log("nice!");
+
+        }).catch((error) => {
+            console.error(error);
+        });
+    }
 
     // canvasList = canvases.map((canvas, index) => {
     //     return <div key={index}>{canvas}</div>
     // })
-
     console.log(canvases.length);
 
     return (
         <div>
             <div className='grid-container'>
                 {canvasList}
+                {console.log("Return:" + canvases.length)}
+                {console.log(canvasList.length)}
             </div>
         </div>
     );
