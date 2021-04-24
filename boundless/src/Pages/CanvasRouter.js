39c0bbcaececd62bd484f515d990a4e4ef162c9a
anujamehta <44733188+anujamehta@users.noreply.github.com>
2021-04-23 21:44:37 -0500

    Loading from database but it doesn't work. Don't access profile page

--- boundless/src/Pages/CanvasRouter.js
+++ boundless/src/Pages/CanvasRouter.js
@@ -3,7 +3,7 @@ import {CanvasPage} from "./CanvasPage";
 import {useSelector} from "react-redux";
 import {selectLoggedIn, selectUsername} from "../Redux/loginState";
 import RequestAccess from "./RequestAccess";
-import firebase from "../Firebase";
+import firebase, {addCanvas} from "../Firebase";
 
 export const CanvasRouter = ({isNew}) => {
     let {path, url} = useRouteMatch();
@@ -15,9 +15,10 @@ export const CanvasRouter = ({isNew}) => {
 
     if (isNew) {
         id = Date.now().toString(); //using current time (in ms) provides unique id in virtually all situations
+        addCanvas(id, username, true, () => {}, () => {
+            console.log("Did not add to DB");
+            });
     }
-
-    console.log("Canvas Router" + id);
 
     return (
         isNew ? <Redirect to={`/canvas/${id}`} /> :
