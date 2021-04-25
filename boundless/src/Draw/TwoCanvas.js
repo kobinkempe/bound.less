67bcc8b92885624d9984df123bab13f365b24b26
anujamehta <44733188+anujamehta@users.noreply.github.com>
2021-04-25 13:55:20 -0500

    Commenting idk

--- boundless/src/Draw/TwoCanvas.js
+++ boundless/src/Draw/TwoCanvas.js
@@ -80,7 +80,7 @@ const TwoCanvas = ({toolInUse,
 
         canvasPath = "/public/" + "canvas_" + canvasID + ".svg";
     }
-    console.log(canvasPath);
+    // console.log(canvasPath);
 
     //Keeps track of the # of shapes that need to be removed from two
     //const [PGroup, setPGroup] = useState(0);
@@ -585,6 +585,7 @@ const TwoCanvas = ({toolInUse,
             case PEN_TYPES[3]:
                 path.stroke = 'white';
                 path.linewidth = radius * 2;
+                break;
             default:
                 break;
         }
